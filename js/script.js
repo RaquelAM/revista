23f8596a89928ac8a6c510b01aae9759adf71ec8
RaquelAM <raquelaguilar@ciencias.unam.mx>
2018-10-14 20:54:41 -0500

    animated scroll

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(".scroll").on("click",function(event){
+  event.preventDefault();
+  var target = $( $(this).attr('href') );
+  $('html, body').animate({
+      scrollTop: target.offset().top
+  }, 500);
+})
