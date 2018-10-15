f176c69a9ecc385b3cfb3f3bb885e870bc7a7323
RaquelAM <raquelaguilar@ciencias.unam.mx>
2018-10-14 20:57:54 -0500

    fixed header

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ $(".scroll").on("click",function(event){
   event.preventDefault();
   var target = $( $(this).attr('href') );
   $('html, body').animate({
-      scrollTop: target.offset().top
+      scrollTop: target.offset().top - 95
   }, 500);
 })
